96118394748f6b3e69b507bb70385b73f1a694f1
I left from github. probably... <sgwr_dts@yahoo.co.jp>
2015-04-17 01:26:08 +0900

    Update active_record-relation.rb

--- lib/arel-mysql-index-hint/active_record-relation.rb
+++ lib/arel-mysql-index-hint/active_record-relation.rb
@@ -1,9 +1,7 @@
 class ActiveRecord::Relation
-  def index_hint(index_hint_by_table)
+  def hint(index_hint_by_table)
     self.visitor.index_hint_by_table = {}.with_indifferent_access
     self.visitor.index_hint_by_table.update(index_hint_by_table)
     self
   end
-
-  alias hint index_hint
 end
