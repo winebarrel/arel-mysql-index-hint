ca89ffd25073b3b70aed4392df734deaeed31fcc
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-04-18 08:43:48 +0900

    Fix clear timing

--- lib/arel-mysql-index-hint/arel-visitors-mysql.rb
+++ lib/arel-mysql-index-hint/arel-visitors-mysql.rb
@@ -1,6 +1,14 @@
 class Arel::Visitors::MySQL
   INDEX_HINT_BY_TABLE_THREAD_KEY = "INDEX_HINT_BY_TABLE_THREAD_KEY"
 
+  def accept_with_index_hint(object)
+    retval = accept_without_index_hint(object)
+    clear_index_hint_by_table
+    retval
+  end
+
+  alias_method_chain :accept, :index_hint
+
   def visit_Arel_Table_with_index_hint(o, a)
     sql = visit_Arel_Table_without_index_hint(o, a)
     index_hint = get_index_hint(o.name)
@@ -18,7 +26,11 @@ class Arel::Visitors::MySQL
 
   def get_index_hint(table)
     index_hint_by_table = Thread.current[INDEX_HINT_BY_TABLE_THREAD_KEY]
-    index_hint_by_table.try(:delete, table)
+    index_hint_by_table.try(:[], table)
+  end
+
+  def clear_index_hint_by_table
+    Thread.current[INDEX_HINT_BY_TABLE_THREAD_KEY] = nil
   end
 
   def append_index_hint(sql, index_hint)
