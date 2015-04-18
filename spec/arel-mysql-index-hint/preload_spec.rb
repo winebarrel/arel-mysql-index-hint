9ed4c6e8297f74005a05899226a7c2d8c3acc6bd
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-04-18 10:54:02 +0900

    Add preload test

---
+++ spec/arel-mysql-index-hint/preload_spec.rb
@@ -0,0 +1,22 @@
+describe "arel-mysql-index-hint" do
+  describe "#preload" do
+    subject do
+      User.
+        where(id: 1).
+        preload(:microposts).
+        hint(users: {index_users_on_email: hint_type}).
+        to_sql
+    end
+
+    let(:sql) do
+      "SELECT `users`.* " +
+      "FROM `users` " +
+      "force INDEX (index_users_on_email)  " +
+      "WHERE `users`.`id` = 1"
+    end
+
+    let(:hint_type) { :force }
+
+    it { is_expected.to eq sql }
+  end
+end
