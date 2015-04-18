16208e3f04d4c22ad70f2796045a6edb0125bf45
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-04-18 11:09:41 +0900

    Add coveralls

--- arel-mysql-index-hint.gemspec
+++ arel-mysql-index-hint.gemspec
@@ -22,4 +22,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "mysql2"
   spec.add_development_dependency "rspec", ">= 3.0.0"
   spec.add_development_dependency "factory_girl"
+  spec.add_development_dependency "coveralls"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,14 @@
+if ENV['TRAVIS']
+  require 'simplecov'
+  require 'coveralls'
+
+  SimpleCov.formatter = Coveralls::SimpleCov::Formatter
+
+  SimpleCov.start do
+    add_filter "spec/"
+  end
+end
+
 require "arel-mysql-index-hint"
 require "models"
 
