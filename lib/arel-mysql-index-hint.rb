83d9ef7dde2a6ee2435391d29959b855274c4516
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-04-18 16:38:12 +0900

    Add require "active_support"

--- lib/arel-mysql-index-hint.rb
+++ lib/arel-mysql-index-hint.rb
@@ -1,3 +1,5 @@
+require "active_support"
+
 ActiveSupport.on_load :active_record do
   require "arel-mysql-index-hint/active_record-hint_methods"
   require "arel-mysql-index-hint/active_record-querying"
