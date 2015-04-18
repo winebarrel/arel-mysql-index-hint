be562451a52eaf699a8009d9714e2b32d085648f
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-04-18 17:41:23 +0900

    Small fix

--- lib/arel-mysql-index-hint.rb
+++ lib/arel-mysql-index-hint.rb
@@ -4,15 +4,15 @@ ActiveSupport.on_load :active_record do
   require "arel-mysql-index-hint/active_record-hint_methods"
   require "arel-mysql-index-hint/arel-visitors-mysql"
 
-  Arel::Visitors::MySQL.class_eval do
-    include ArelMysqlIndexHint::ArelVisitorsMySQL
-  end
-
   ActiveRecord::Relation.class_eval do
     include ArelMysqlIndexHint::ActiveRecordHintMethods
   end
 
   ActiveRecord::Querying.class_eval do
     delegate :hint, :to => :all
+  end
+
+  Arel::Visitors::MySQL.class_eval do
+    include ArelMysqlIndexHint::ArelVisitorsMySQL
   end
 end
