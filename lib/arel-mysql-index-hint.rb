d91cd22812146e85fbb6ddc77596ba412bc0682c
Genki Sugawara <sgwr_dts@yahoo.co.jp>
2015-04-18 10:34:22 +0900

    Fix require

--- lib/arel-mysql-index-hint.rb
+++ lib/arel-mysql-index-hint.rb
@@ -1,6 +1,4 @@
-require "active_support/core_ext"
 require "active_record"
-require "arel"
 
 require "arel-mysql-index-hint/active_record-hint_methods"
 require "arel-mysql-index-hint/active_record-querying"
