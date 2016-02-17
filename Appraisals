bb3b257c3f2ad1f7361be0a7585ccb83f4baa2e0
winebarrel <sgwr_dts@yahoo.co.jp>
2016-02-18 03:04:34 +0900

    Fix spec

--- Appraisals
+++ Appraisals
@@ -1,11 +1,13 @@
 appraise "activerecord-4.0" do
-  gem "activerecord", "4.0.13"
+  gem "activerecord", "~> 4.0.0"
+  gem "mysql2", "~> 0.3.10"
 end
 
 appraise "activerecord-4.1" do
-  gem "activerecord", "4.1.10"
+  gem "activerecord", "~> 4.1.0"
+  gem "mysql2", "~> 0.3.10"
 end
 
 appraise "activerecord-4.2" do
-  gem "activerecord", "4.2.1"
+  gem "activerecord", "~> 4.2.0"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,12 @@ require "models"
 
 $__arel_mysql_index_hint_sql_log__ = []
 
+TEST_MYSQL_HOST = '127.0.0.1'
+TEST_MYSQL_PORT = 3306
+TEST_MYSQL_USER = 'root'
+TEST_MYSQL_PASS = 'password'
+MYSQL_CLI = "MYSQL_PWD=#{TEST_MYSQL_PASS} mysql -h #{TEST_MYSQL_HOST} -P #{TEST_MYSQL_PORT} -u #{TEST_MYSQL_USER}"
+
 ActiveSupport::Notifications.subscribe('sql.active_record') do |name, start, finish, id, payload|
   sql = payload[:sql]
   $__arel_mysql_index_hint_sql_log__ << sql.gsub(/\s+/, " ") if sql
@@ -26,7 +32,11 @@ RSpec.configure do |config|
 
     ActiveRecord::Base.establish_connection(
       adapter: 'mysql2',
-      database: 'arel_mysql_index_hint_test'
+      database: 'arel_mysql_index_hint_test',
+      host: TEST_MYSQL_HOST,
+      port: TEST_MYSQL_PORT,
+      username: TEST_MYSQL_USER,
+      password: TEST_MYSQL_PASS,
     )
   end
 
@@ -37,7 +47,7 @@ end
 
 def init_database
   sql_file = File.expand_path('../init.sql', __FILE__)
-  system("mysql -uroot < #{sql_file}")
+  system("#{MYSQL_CLI} < #{sql_file}")
 end
 
 def sql_log
